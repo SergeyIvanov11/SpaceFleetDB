48712373ea5de1e513e1724e2c402559be1bebe4
user <1539ivanov@mail.ru>
2025-12-10 21:18:11 +0300

    Initial commit

---
+++ src/main/java/com/example/spacefleetdb/SpaceFleetDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacefleetdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFleetDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceFleetDbApplication.class, args);
+    }
+
+}
